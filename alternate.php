fab56176f8e83945394ee68cc4eed1100ee5f331
Dzaki Ahnaf Z <61301953+dzaki236@users.noreply.github.com>
2022-12-02 21:08:41 +0700

    update syntax

--- alternate.php
+++ alternate.php
@@ -1,5 +1,6 @@
 <?php
 // This is for newest version of carbon 
-use Carbon/Carbon;
-$date = Carbon::parse(date('2022-11-15'))->locale('id')->translatedFormat('l, j F Y');       
-echo $date."/n";
+require "vendor/autoload.php";
+use Carbon\Carbon;
+$date = Carbon::parse(date('2022-11-15'))->locale('id');       
+echo $date->translatedFormat('l, j F Y')."/n";
